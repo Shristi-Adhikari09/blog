c10368ff7b69f663db443b978a72d82fd3fd5e85
Shristi-Adhikari09 <adh09shristi@gmail.com>
2025-06-15 13:04:12 +0545

    feat:form submit and navigate to userblog

--- src/pages/CreateBlog/index.jsx
+++ src/pages/CreateBlog/index.jsx
@@ -1,17 +1,120 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
+import { postBlog } from "../../services";
+import { useNavigate } from "react-router";
 
-export default function CreateBlog(){
-    const { logout }=useContext(AuthContext)
-    const logoutHandler=() =>{
-        localStorage.removeItem('token');
-        logout();
-    };
+const blogFields = [
+  { name: "Title", id: "title", type: "input", inputType: "text" },
+  { name: "Summary", id: "summary", type: "textarea" },
+  { name: "Author", id: "author", type: "input", inputType: "text" },
+  { name: "Created At", id: "created_at", type: "input", inputType: "date" },
+  {
+    name: "Featured",
+    id: "featured",
+    type: "radio",
+    options: ["true", "false"],
+  },
+  { name: "Body", id: "body", type: "textarea" },
+  { name: "Tags", id: "tags", type: "input", inputType: "text" },
+];
 
-    return ( 
-   <div>
-        <button 
-        className="cursor-pointer  p-1.5 bg-gray-200 rounded-lg" type="button" onClick={logoutHandler }>Logout</button>
+export default function CreateBlog() {
+  const { logout } = useContext(AuthContext);
+  const [blogData, setBlogData] = useState({});
+  const navigate = useNavigate()
+
+  const logoutHandler = () => {
+    localStorage.removeItem("token");
+    logout();
+  };
+
+  const handleBlogFormSubmit = async (e) => {
+    e.preventDefault();
+   await postBlog(blogData);
+   navigate('/user-blog');
+  };
+
+  return (
+    <div>
+      <button
+        className="cursor-pointer px-2 py-1 bg-gray-200 rounded-lg"
+        type="button"
+        onClick={logoutHandler}
+      >
+        Logout
+      </button>
+      <h2 className="text-2xl font-semibold">Add new blog</h2>
+
+      <form
+        onSubmit={handleBlogFormSubmit}
+        className="flex flex-col md:w-1/2 w-full mx-auto"
+      >
+        {blogFields.map((field) => (
+          <div className="flex flex-col gap-1 mb-4" key={field.id}>
+            <label htmlFor={field.id}>{field.name}</label>
+
+            {field.type === "input" && (
+              <input
+                type={field.inputType}
+                id={field.id}
+                name={field.id}
+                value={blogData[field.id] }
+                onChange={(e) =>
+                  setBlogData((prev) => ({
+                    ...prev,
+                    [field.id]: e.target.value,
+                  }))
+                }
+                className="border border-gray-400 h-8 rounded-lg p-2"
+              />
+            )}
+
+            {field.type === "textarea" && (
+              <textarea
+                id={field.id}
+                name={field.id}
+                value={blogData[field.id] }
+                onChange={(e) =>
+                  setBlogData((prev) => ({
+                    ...prev,
+                    [field.id]: e.target.value,
+                  }))
+                }
+                className="border border-gray-400 rounded-lg p-2"
+              />
+            )}
+
+            {field.type === "radio" && (
+              <div className="flex gap-4">
+                {field.options.map((option) => (
+                  <label  className="capitalize" key={option} >
+                    <input
+                      type="radio"
+                      name={field.id}
+                      value={option}
+                      checked={blogData[field.id] === option}
+                      onChange={(e) =>
+                        setBlogData((prev) => ({
+                          ...prev,
+                          [field.id]: e.target.value,
+                        }))
+                      }
+                    />
+                    {option}
+                  </label>
+                ))}
+              </div>
+            )}
+          </div>
+        ))}
+
+        <button
+          type="submit"
+          className=" w-30  mx-auto mt-4 bg-blue-500 text-white px-4 py-2 rounded-lg"
+        >
+          Submit Blog
+        </button>
+      </form>
     </div>
-    );
+  );
 }
--- src/services/index.js
+++ src/services/index.js
@@ -39,9 +39,29 @@
         }
 
     } catch (err) {
-        throw new Error(err.message);  // safer error
+        throw new Error(err);  // safer error
     }
 }
+  export const postBlog = async (body) => {
+    try {
+        const res = await fetch("http://localhost:3000/blog", {
+        body : JSON.stringify(body),
+        method: "POST",
+        headers:{ "Content-Type": "application/json", authorization: `Bearer ${localStorage.getItem('token')}` }
+    })
+        const data = await res.json();
+
+        if (res.ok) {
+            return data;
+        } else {
+            throw new Error(data.message);
+        }
+
+    } catch (err) {
+        throw new Error(err);  // safer error
+    }
+}
+  
 
   export const logout = async () => {
     try{
